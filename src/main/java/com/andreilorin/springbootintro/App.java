b38174c29ebe9acafe202083996b096ebb8e14f7
andrei lorin <andreilorin@gmail.com>
2017-05-08 21:48:49 +0100

    second tutorial

--- src/main/java/com/andreilorin/springbootintro/App.java
+++ src/main/java/com/andreilorin/springbootintro/App.java
@@ -1,12 +1,19 @@
 package com.andreilorin.springbootintro;
 
-import org.springframework.boot.SpringApplication;
+import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+import com.andreilorin.springbootintro.data.Data;
 
 @SpringBootApplication
 public class App {
+	
+	private static final Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        RestTemplate restTemplate = new RestTemplate();
+        Data data = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Data.class);
+        logger.info(data.toString());
     }
 }
